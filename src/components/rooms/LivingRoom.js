d49f76fdeaf357606893de6141aa88a1854bb9f5
Matijahh <matijaradofficial@gmail.com>
2020-12-13 18:41:28 +0100

    dasdas

--- src/components/rooms/LivingRoom.js
+++ src/components/rooms/LivingRoom.js
@@ -3,12 +3,6 @@ import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
 
-import lightBulb from "../../assets/images/light-bulb.svg";
-import ccTv from "../../assets/images/cctv.svg";
-import television from "../../assets/images/television.svg";
-import lockDoors from "../../assets/images/doorknob.svg";
-import DeviceInsight from "./DeviceInsight";
-
 class LivingRoom extends Component {
   render() {
     const { devices } = this.props;
@@ -16,95 +10,35 @@ class LivingRoom extends Component {
       <div className="container rooms-container">
         <div className="center-alignment">
           <div className="row">
-            <div className="col s12 m6">
-              <div className="card device-box border-radius green darken-3">
-                <img src={lightBulb} alt="Light" className="device-icon" />
-                <div className="device-info">
-                  <div class="switch">
-                    <label>
-                      <input type="checkbox" />
-                      <span class="lever"></span>
-                      Turn on/off the lights
-                    </label>
+            {devices &&
+              devices.map((device) => {
+                return (
+                  <div className="col s12 m6">
+                    <div className="card device-box border-radius green darken-3">
+                      <img
+                        src={device.src}
+                        alt="Device"
+                        className="device-icon"
+                      />
+                      <div className="device-info">
+                        <div class="switch">
+                          <label>
+                            <input type="checkbox" />
+                            <span class="lever"></span>
+                            Turn on/off the heat
+                          </label>
+                        </div>
+                        <button
+                          type="text"
+                          className="waves-effect waves-light btn green darken-1 white-text"
+                        >
+                          See the insights
+                        </button>
+                      </div>
+                    </div>
                   </div>
-                  <button
-                    type="text"
-                    className="waves-effect waves-light btn green darken-1 white-text"
-                    onClick={() => {
-                      return <DeviceInsight />;
-                    }}
-                  >
-                    See the insights
-                  </button>
-                </div>
-              </div>
-            </div>
-            <div className="col s12 m6">
-              <div className="card device-box border-radius green darken-3">
-                <img
-                  src={television}
-                  alt="Television"
-                  className="device-icon"
-                />
-                <div className="device-info">
-                  <div class="switch">
-                    <label>
-                      <input type="checkbox" />
-                      <span class="lever"></span>
-                      Turn on/off the TV
-                    </label>
-                  </div>
-                  <button
-                    type="text"
-                    className="waves-effect waves-light btn green darken-1 white-text"
-                  >
-                    See the insights
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col s12 m6">
-              <div className="card device-box border-radius green darken-3">
-                <img src={ccTv} alt="CcTv" className="device-icon" />
-                <div className="device-info">
-                  <div class="switch">
-                    <label>
-                      <input type="checkbox" />
-                      <span class="lever"></span>
-                      Turn on/off the cameras
-                    </label>
-                  </div>
-                  <button
-                    type="text"
-                    className="waves-effect waves-light btn green darken-1 white-text"
-                  >
-                    See the insights
-                  </button>
-                </div>
-              </div>
-            </div>
-            <div className="col s12 m6">
-              <div className="card device-box border-radius green darken-3">
-                <img src={lockDoors} alt="Lock" className="device-icon" />
-                <div className="device-info">
-                  <div class="switch">
-                    <label>
-                      <input type="checkbox" />
-                      <span class="lever"></span>
-                      Lock the doors
-                    </label>
-                  </div>
-                  <button
-                    type="text"
-                    className="waves-effect waves-light btn green darken-1 white-text"
-                  >
-                    See the insights
-                  </button>
-                </div>
-              </div>
-            </div>
+                );
+              })}
           </div>
         </div>
       </div>
@@ -113,9 +47,8 @@ class LivingRoom extends Component {
 }
 
 const mapStateToProps = (state) => {
-  const devices = state.firestore.data.devices;
   return {
-    devices: devices,
+    devices: state.firestore.ordered.devices,
     auth: state.firebase.auth,
   };
 };
